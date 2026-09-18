f3b08ddfc22a273229ca522a3e503e37c27d0190
ahmed elbardawell <brdweelahmed@gmail.com>
2026-09-19 00:23:27 +0300

    Add iOS Google Sign In placeholders

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -13,6 +13,7 @@ Future<void> initDI() async {
   // External
   final supabase = Supabase.instance.client;
   final googleSignIn = GoogleSignIn(
+    clientId: const String.fromEnvironment('IOS_CLIENT_ID', defaultValue: 'YOUR_IOS_CLIENT_ID.apps.googleusercontent.com'),
     serverClientId: '638779791094-np42vq80lks1pk7nrg5iq10inktd1th7.apps.googleusercontent.com',
   );
 
